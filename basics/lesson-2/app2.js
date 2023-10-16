35ef86566d05756ea46b8d10eb12f86c69682c5c
manipriyan77 <manipriyangopalan@gmail.com>
2023-10-16 22:16:54 +0530

    started learning computed properties

---
+++ basics/lesson-2/app2.js
@@ -0,0 +1,68 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name: '',
+      myFullname: '',
+    };
+  },
+  watch: {
+    name(value) {
+      if (value === '') {
+        this.myFullname === '';
+      } else {
+        //   this.myFullname = value + ' ' + 'GS';
+
+        this.myFullname = this.name + ' ' + 'GS';
+      }
+    },
+  },
+  computed: {
+    fullName() {
+      console.log('renderinf');
+
+      if (this.name === '') {
+        return '';
+      }
+      return this.name + ' ' + 'GS';
+    },
+  },
+  methods: {
+    addCount(num) {
+      console.log('add');
+
+      this.counter = this.counter + num;
+    },
+    reduceCount(num) {
+      console.log('reduce');
+
+      this.counter = this.counter - num;
+    },
+    setName(e, lastName) {
+      console.log('setNasme');
+
+      this.name = e.target.value;
+    },
+    submitForm(event) {
+      console.log('form');
+
+      event.preventDefault();
+    },
+
+    resetInput() {
+      console.log('reset');
+
+      this.name = '';
+    },
+    getFullName() {
+      console.log('fullname');
+
+      if (this.name === '') {
+        return '';
+      }
+      return this.name + ' ' + 'GS';
+    },
+  },
+});
+
+app.mount('#events');
