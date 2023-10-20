87ea3a27e4dbc1783ded7a416d0d2c075e8d388e
manipriyan77 <manipriyangopalan@gmail.com>
2023-10-20 19:49:27 +0530

    started working with VUE create app

---
+++ basics/lesson-4/src/components/FriendContact.vue
@@ -0,0 +1,34 @@
+<template>
+  <li>
+    <h2>{{ name }}</h2>
+    <button @click="toggleDetails">Show Details</button>
+    <ul v-if="detailsAreVisible">
+      <li>Address: {{ address }}</li>
+      <li>phone-number: {{ phoneNumber }}</li>
+      <li>Favurite: {{ isFavourite }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  //   props: ['name', 'address', 'phoneNumber'],
+  props: {
+    name: String,
+    address: String,
+    phoneNumber: String,
+    isFavourite: String,
+  },
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: { id: 'mani', name: 'mani', address: 'Chennai' },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
