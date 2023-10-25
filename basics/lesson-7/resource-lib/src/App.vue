018ac4f87bdf5e0591883817ac60802ed5176d31
manipriyan77 <manipriyangopalan@gmail.com>
2023-10-25 22:26:19 +0530

    started wotking with resource handler

---
+++ basics/lesson-7/resource-lib/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <TheHeader title="Remember me"></TheHeader>
+  <StoredResource :resources="storedResources"></StoredResource>
+</template>
+
+<script>
+import StoredResource from './components/learning-resources/StoredResource.vue';
+import TheHeader from './components/layouts/theheader.vue';
+
+export default {
+  components: {
+    StoredResource,
+    TheHeader,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
